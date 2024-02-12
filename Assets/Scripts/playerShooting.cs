026034278834cb8508095a53a53fdba2dd227b81
Adham Saleh <144182454+JustWinStudios@users.noreply.github.com>
2024-02-12 01:35:20 -0500

    Completed the playerShooting script

--- Assets/Scripts/playerShooting.cs
+++ Assets/Scripts/playerShooting.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class playerShooting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 10f;
+    public KeyCode fireKey = KeyCode.Space;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        // Check for input each frame
+        if (Input.GetKeyDown(fireKey))
+        {
+            Debug.Log("Fire key pressed"); // Confirm that the key press is detected
+
+            // Instantiate the projectile at the player's position with no rotation
+            GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+
+            // Try to get the Rigidbody2D component from the instantiated projectile
+            Rigidbody2D rb = projectile.GetComponent<Rigidbody2D>();
+
+            if (rb != null)
+            {
+                // Apply force to the projectile's Rigidbody2D
+                rb.AddForce(transform.right * projectileSpeed, ForceMode2D.Impulse);
+                Debug.Log("Force applied to projectile");
+            }
+            else
+            {
+                Debug.LogError("Projectile prefab does not have a Rigidbody2D component attached.");
+            }
+        }
     }
 }
