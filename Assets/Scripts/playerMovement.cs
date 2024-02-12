4aa70a1a761d8b05aaeb5e022b5b270a2f85fb3b
Adham Saleh <144182454+JustWinStudios@users.noreply.github.com>
2024-02-11 20:32:43 -0500

    Created the logic for player movement
    This is a simple script that uses WASD to move the player around. Gravity is set to 0.

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Movement Speed
+    public float speed = 5f;
+
+    // Rigidbody Reference
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Get Movement Input (WASD KEYS)
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // Combine input into a movment vector:
+        Vector2 movement = new Vector2(horizontalInput, verticalInput);
+
+        // Apply movement to Rigidbody
+        rb.velocity = movement * speed;
     }
 }
