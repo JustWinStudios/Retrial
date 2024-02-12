4637c25f64896b29f6e26f02c7b3ef27796d7356
Adham Saleh <144182454+JustWinStudios@users.noreply.github.com>
2024-02-12 11:02:00 -0500

    successfully consolidated all player related code into one playerManager

--- Assets/Scripts/playerManager.cs
+++ Assets/Scripts/playerManager.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class playerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Movement properties
+    public float speed = 5f;
+    private Rigidbody2D rb;
+
+    // Shooting properties
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 10f;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleMovement();
+        HandleShooting();
+    }
+
+    void HandleMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(horizontalInput, verticalInput);
+        rb.velocity = movement * speed;
+    }
+
+    void HandleShooting()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left mouse button
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 shootingDirection = (mousePosition - (Vector2)transform.position).normalized;
+            GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            Rigidbody2D projectileRb = projectile.GetComponent<Rigidbody2D>();
+            projectileRb.velocity = shootingDirection * projectileSpeed;
+        }
     }
 }
