8b9edc4385ba0d695b126608a02781a987239dae
Adham Saleh <144182454+JustWinStudios@users.noreply.github.com>
2024-02-12 01:38:23 -0500

    Started projectileManager script and gave enemy the enemy tag

--- Assets/Scripts/projectileManager.cs
+++ Assets/Scripts/projectileManager.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class projectileManager : MonoBehaviour
+public class ProjectileManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float lifespan = 10f; // Time after which the projectile will be destroyed
+    public int damage = 10; // Damage dealt by the projectile
+
+    private Vector2 startPosition;
+
     void Start()
     {
-        
+        startPosition = transform.position;
+        Destroy(gameObject, lifespan); // Automatically destroy the projectile after its lifespan
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(startPosition, transform.position) >= 10f)
+        {
+            Destroy(gameObject); // Destroy the projectile if it moves 10 units from its starting point
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")) // Ensure your enemy GameObject has the tag "Enemy"
+        {
+            EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damage);
+            }
+            Destroy(gameObject); // Destroy the projectile upon hitting an enemy
+        }
     }
 }
